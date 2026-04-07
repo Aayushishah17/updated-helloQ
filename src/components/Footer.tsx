b792fbd8445d41f2457bd14f0236f7a0c37e7ee8
aayushishah1711 <aayushi1117shah@gmail.com>
2026-04-07 16:28:03 +0530

    final2

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -66,7 +66,7 @@ const cols = [
     title: "Social",
     links: [
       { name: "Instagram", path: "https://www.instagram.com/thehelloq" },
-      { name: "Facebook", path: "https://www.facebook.com/share/1EVsm2JkeD/" },
+      { name: "Facebook", path: "https://www.facebook.com/people/HelloQ/61574389953708/" },
       { name: "Snapchat", path: "https://www.snapchat.com/@thehelloq" },
       { name: "Youtube", path: "#" },
     ],
